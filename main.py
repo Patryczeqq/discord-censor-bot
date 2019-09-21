302f89d32c2afd991c8254da7a3cbd81e40d9602
Charlie Thomas <charliepthomas@live.co.uk>
2019-09-21 14:27:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,29 +9,36 @@ def loadConfig(*configs):
         with open("config.txt") as file:
             line = file.readline()
             while line != "":
-                if line[0] == "#" and line[1:-1] == config:
-                    line = file.readline()
-                    settings = []
-                    while line[0] != "#":
-                        settings.append(line[:-1])
+                if line[0] == "#":
+                    if line[1:-1] == config:
                         line = file.readline()
-                    output.append(settings)
-                    line = ""
+                        settings = []
+                        while line[0] != "#":
+                            settings.append(line[:-1])
+                            line = file.readline()
+                        output.append(settings)
+                        line = ""
+                    elif config in line:
+                        output.append(line[line.find("=")+1:-1])
+                        line = ""
+                    else:
+                        line = file.readline()
                 else:
                     line = file.readline()
     return output
 
-bannedWords, exemptChannels = loadConfig("banned words", "exempt channels")
+bannedWords, exemptChannels, prefix = loadConfig("banned words", "exempt channels", "prefix")
 client = discord.Client()
 
 @client.event
 async def on_message(message):
     print(message.channel.name)
-    if message.channel.name not in exemptChannels:
+    
+    elif message.channel.name not in exemptChannels:
         text = re.sub("[^a-z]","",message.content.lower())
         if any(bannedWord in text for bannedWord in bannedWords):
             await message.delete()
-    if re.match("is(\s\d+\.?\d*\sinch(es)?\senough\??|.+\stoo\s(short|small)\??)",message.content.lower()):
+    elif re.match("is(\s\d+\.?\d*\sinch(es)?\senough\??|.+\stoo\s(short|small)\??)",message.content.lower()):
         await message.channel.send("https://cdn.discordapp.com/attachments/148491526073221120/622438991924297738/unknown.png")
 
 token = os.environ.get("DISCORD_BOT_SECRET")
